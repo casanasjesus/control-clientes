3ab74861c8ed5aa1ab5f17c0a4c268ddfdf8ecbc
Jesús Casañas <casanasjesus97@gmail.com>
2025-06-12 15:13:19 -0300

    firebase configuration

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyB7XnCCpjok0t6TMLyVRDlCJiaebN2VTJ8',
+    authDomain: 'control-clientes-56ec3.firebaseapp.com',
+    projectId: 'control-clientes-56ec3',
+    storageBucket: 'control-clientes-56ec3.firebasestorage.app',
+    messagingSenderId: '89562412616',
+    appId: '1:89562412616:web:cefe9b060a5e266d3d4a7d',
+  },
+};
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,21 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { provideHttpClient } from '@angular/common/http';
+import { environment } from './environments/environment';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideStorage, getStorage } from '@angular/fire/storage';
 
-bootstrapApplication(App, appConfig).catch((err) => console.error(err));
+bootstrapApplication(App, {
+  ...appConfig,
+  providers: [
+    provideHttpClient(),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideFirestore(() => getFirestore()),
+    provideAuth(() => getAuth()),
+    provideStorage(() => getStorage()),
+    ...appConfig.providers,
+  ],
+}).catch((err) => console.error(err));
