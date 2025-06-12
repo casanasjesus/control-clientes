7392580d77728749dc96552310df175edc12351e
Jesús Casañas <casanasjesus97@gmail.com>
2025-06-12 15:46:16 -0300

    plantilla y component customers

--- src/app/components/customers/customers.ts
+++ src/app/components/customers/customers.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { Customer } from '../../models/Customer.model';
+import { CustomerService } from '../../services/customer';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-customers',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './customers.html',
-  styleUrl: './customers.css',
 })
-export class CustomersComponent {}
+export class CustomersComponent {
+  customers: Customer[] | null = null;
+
+  constructor(private readonly customerService: CustomerService) {}
+
+  ngOnInit() {
+    this.customerService.getCustomers().subscribe((customers: Customer[]) => {
+      this.customers = customers;
+    });
+  }
+}
